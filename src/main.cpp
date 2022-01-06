d8b0c440b21b33a3b5fcfc02c7e902369a8b6923
BuildTools <unconfigured@null.spigotmc.org>
2022-01-05 22:49:27 -0500

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   build/main.exe
    	modified:   src/main.cpp
    - We've got a working cuboid that rotates with keystrokes!

--- src/main.cpp
+++ src/main.cpp
@@ -64,16 +64,24 @@ void MultiplyMatrixVector(vec3d &i, vec3d &o, mat4x4 &m)
 
 }
 
-void DrawTriangle(SDL_Renderer *renderer, float x1, float y1, float x2, float y2, float x3, float y3, SDL_Color col)
+void DrawTriangle(SDL_Renderer *renderer, vec2d vert1, vec2d vert2, vec2d vert3, SDL_Color col)
 {
-	SDL_RenderDrawLineF(renderer, x1, y1, x2, y2);
-	SDL_RenderDrawLineF(renderer, x2, y2, x3, y3);
-	SDL_RenderDrawLineF(renderer, x3, y3, x1, y1);
+	
+	SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, col.a);
+
+	vert1 = cart_to_screen(vert1);
+	vert2 = cart_to_screen(vert2);
+	vert3 = cart_to_screen(vert3);
+	SDL_RenderDrawLineF(renderer, vert1.x, vert1.y, vert2.x, vert2.y);
+	SDL_RenderDrawLineF(renderer, vert2.x, vert2.y, vert3.x, vert3.y);
+	SDL_RenderDrawLineF(renderer, vert3.x, vert3.y, vert1.x, vert1.y);
 }
 
 int main(int argv, char** args)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
+
+	float fTheta, tTheta=45.0f;
 
 	// Projection Matrix
 	float fNear = 0.1;
@@ -82,7 +90,7 @@ int main(int argv, char** args)
 	float fAspectRatio = (float)SCREEN_H/(float)SCREEN_W;
 	float fFOV_rad = 1.0/(SDL_tanf((fFOV/2)*(3.14159265/180.0)));
 
-	mat4x4 matProj;  //[row][column]
+	mat4x4 matProj, mat_XRot, mat_ZRot;  //[row][column]
 	matProj.m[0][0] = fAspectRatio*fFOV_rad;
 	matProj.m[1][0] = 0.0;
 	matProj.m[2][0] = 0.0;
@@ -100,6 +108,9 @@ int main(int argv, char** args)
 	matProj.m[2][3] = 1.0;
 	matProj.m[3][3] = 0.0;
 	
+
+
+
 	// define cube in 3D space made up of triangles.  tris are drawn in clockwise order when facing the triangle
 	mesh meshCube;
 	meshCube.tris = {
@@ -149,6 +160,30 @@ int main(int argv, char** args)
 				{
 					isRunning = false;
 				}
+
+				if (event.key.keysym.sym == SDLK_RIGHT)
+				{
+					fTheta +=1;
+					
+				}
+				
+				if (event.key.keysym.sym == SDLK_LEFT)
+				{
+					fTheta -=1;
+					
+				}
+
+				if (event.key.keysym.sym == SDLK_UP)
+				{
+					tTheta +=1;
+					
+				}
+				
+				if (event.key.keysym.sym == SDLK_DOWN)
+				{
+					tTheta -=1;
+					
+				}
 			}
 		}
 		
@@ -156,47 +191,82 @@ int main(int argv, char** args)
 		SDL_RenderClear(renderer);
 
 
-		
-		/*vec2d line0, line1, single_point;
-		line0.x = -1;
-		line0.y = -.9;
-		line1.x=1;
-		line1.y=.9;
+		// Z Rotation Matrix
+		mat_ZRot.m[0][0] = SDL_cosf(fTheta*(3.14159265/180.0));
+		mat_ZRot.m[0][1] = SDL_sinf(fTheta*(3.14159265/180.0));
+		mat_ZRot.m[1][0] = -SDL_sinf(fTheta*(3.14159265/180.0));
+		mat_ZRot.m[1][1] = SDL_cosf(fTheta*(3.14159265/180.0));
+		mat_ZRot.m[2][2] = 1;
+		mat_ZRot.m[3][3] = 1;
 
-
-		
-		line0 = cart_to_screen(line0);
-		line1 = cart_to_screen(line1);
-		
-		*/
-		
-		// Draw Reticle
-		vec2d single_point;
-		single_point.x=.5;
-		single_point.y=-.5;
-		single_point = cart_to_screen(single_point);
-		
+		// X Rotation Matrix
+		mat_XRot.m[0][0] = 1;
+		mat_XRot.m[1][1] = SDL_cosf(tTheta/2*(3.14159265/180.0));
+		mat_XRot.m[1][2] = SDL_sinf(tTheta/2*(3.14159265/180.0));
+		mat_XRot.m[2][1] = -SDL_sinf(tTheta/2*(3.14159265/180.0));
+		mat_XRot.m[2][2] = SDL_cosf(tTheta/2*(3.14159265/180.0));
+		mat_XRot.m[3][3] = 1;		
+	
 		SDL_Color col;
 		col.r=255; col.g=0; col.b=0; col.a = 255;
 		SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, col.a);
-		//SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-		//SDL_RenderDrawLineF(renderer, line0.x, line0.y, line1.x, line1.y);
 
 		// Draw Triangles
 		for (auto tri: meshCube.tris)
 		{
-			triangle triProjected;
-			MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
-			MultiplyMatrixVector(tri.p[1], triProjected.p[1], matProj);
-			MultiplyMatrixVector(tri.p[2], triProjected.p[2], matProj);
+			// Apply Perspective Projection Matrix
+			triangle triProjected, triTranslated , triRotated_Z, triRotated_ZX;
+			
+			MultiplyMatrixVector(tri.p[0], triRotated_Z.p[0], mat_ZRot);
+			MultiplyMatrixVector(tri.p[1], triRotated_Z.p[1], mat_ZRot);
+			MultiplyMatrixVector(tri.p[2], triRotated_Z.p[2], mat_ZRot);
+			
+			MultiplyMatrixVector(triRotated_Z.p[0], triRotated_ZX.p[0], mat_XRot);
+			MultiplyMatrixVector(triRotated_Z.p[1], triRotated_ZX.p[1], mat_XRot);
+			MultiplyMatrixVector(triRotated_Z.p[2], triRotated_ZX.p[2], mat_XRot);			
+
+			triTranslated = triRotated_ZX;
+			triTranslated.p[0].z = triTranslated.p[0].z+1.0;
+			triTranslated.p[1].z = triTranslated.p[1].z+1.0;
+			triTranslated.p[2].z = triTranslated.p[2].z+1.0;
+
+			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+
+			// Drop 3D to 2D
+			vec2d point1, point2, point3;
+
+			point1.x = triProjected.p[0].x;
+			point1.y = triProjected.p[0].y;
+
+			point2.x = triProjected.p[1].x;
+			point2.y = triProjected.p[1].y;
+
+			point3.x = triProjected.p[2].x;
+			point3.y = triProjected.p[2].y;			
+
+			//fTheta += 1.0f * fElapsedTime;
+
+			// Draw the Triangle!
+			DrawTriangle(renderer,point1, point2, point3,col);
 
 
 		}
 
+		vec2d vert1; vec2d vert2; vec2d vert3;
+		vert1.x=-.40; vert1.y=-.40;
+		vert2.x=.220; vert2.y=-.610;
+		vert3.x=-.200; vert3.y=.700;
+		//DrawTriangle(renderer, vert1, vert2, vert3, col);
 
-		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-
+		// Draw Reticle
+		vec2d single_point;
+		single_point.x=0;
+		single_point.y=0;
+		single_point = cart_to_screen(single_point);
 
 		SDL_RenderDrawPointF(renderer, single_point.x, single_point.y);
 
