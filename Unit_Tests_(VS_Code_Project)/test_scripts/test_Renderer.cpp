061eef9fb2db458ad7ec228f062bc30242f1993b
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-01-31 22:00:48 -0500

    On branch Renderer-Unit-Test-and-Integration  Your branch is up to date with 'origin/Renderer-Unit-Test-and-Integration'.
     Changes to be committed:
    	modified:   Design Diagrams/Class Diagram.dia
    	new file:   Design Diagrams/Class Diagram.dia~
    	modified:   Unit_Tests_(VS_Code_Project)/.vscode/tasks.json
    	new file:   Unit_Tests_(VS_Code_Project)/test_scripts/test_Renderer.cpp
    - Started unit test, then placed aside to test Mesh, and Meshmodifier
      first, possible also MeshPipeline.  Will address Renderer later
    - UML Disgram edits for Vec2d, Vec3d, Triangle, and Mesh

---
+++ Unit_Tests_(VS_Code_Project)/test_scripts/test_Renderer.cpp
@@ -0,0 +1,14 @@
+#include <SDL2/SDL.h>
+#include "Renderer.h"
+
+int main (int argc, char *argv[]){
+    
+    SDL_Init(SDL_INIT_EVERYTHING);
+    Renderer Engine_Renderer;
+
+    
+
+    SDL_Quit();
+
+    return 0;
+}
