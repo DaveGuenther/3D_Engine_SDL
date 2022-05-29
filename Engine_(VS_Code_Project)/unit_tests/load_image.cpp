984711ee9bb7668c20dcb49b09aff2e213a7465f
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-05-29 07:50:26 -0400

    On branch 55-load-texture-images-into-memory-png-or-tga-and-normalize-points-between-0-1  Your branch is ahead of 'origin/55-load-texture-images-into-memory-png-or-tga-and-normalize-points-between-0-1' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   Engine_(VS_Code_Project)/.vscode/tasks.json
    	new file:   Engine_(VS_Code_Project)/build/colorgrid.png
    	new file:   Engine_(VS_Code_Project)/unit_tests/colorgrid.png
    	modified:   Engine_(VS_Code_Project)/unit_tests/load_image.cpp
    - Ued SDL2_Image addon library to load .PNG files as SDL Surface
    - Will next figure out how to normalize dimensions of the image between
      0-1 (U,V) and plot pixels based on lerping U and V

--- Engine_(VS_Code_Project)/unit_tests/load_image.cpp
+++ Engine_(VS_Code_Project)/unit_tests/load_image.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "SDL.h"
-//#include "SDL2/SDL_image.h"
+#include "SDL_image.h"
 
 
 int main (int argv, char** args){
@@ -8,6 +8,19 @@ int main (int argv, char** args){
     SDL_Renderer *renderer;
 	window = SDL_CreateWindow("3D Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 380, 0);
 	renderer = SDL_CreateRenderer(window, -1, 0);
+    
+    IMG_Init(IMG_INIT_PNG);
+    	
+    // load sample.png into image
+    SDL_Surface *image;
+    image=IMG_Load("colorgrid.png");
+    if(!image) {
+        printf("IMG_Load: %s\n", IMG_GetError());
+        // handle error
+    }
+
+    SDL_Texture* ourPNG=SDL_CreateTextureFromSurface(renderer, image);
+
     bool game_is_running=true;
     while(game_is_running){
         SDL_Event event;
@@ -16,8 +29,21 @@ int main (int argv, char** args){
                 game_is_running=false;
             }
         }
-    }
+    
+    SDL_SetRenderDrawColor(renderer,0,0,255,255);
+    SDL_RenderClear(renderer);
 
+    SDL_RenderCopy(renderer,ourPNG, NULL, NULL);
+
+    SDL_RenderPresent(renderer);
+
+
+    }
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_FreeSurface(image);
+    SDL_DestroyTexture(ourPNG);
+    SDL_Quit();
 
     return 0;
 }
