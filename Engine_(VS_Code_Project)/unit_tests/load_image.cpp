786e8bda5a7b41ce0638cef6c0bd7e5d238d14ae
Dave Guenther <dave.j.guenther.stuff@gmail.com>
2022-05-29 00:02:13 -0400

    On branch 55-load-texture-images-into-memory-png-or-tga-and-normalize-points-between-0-1  Your branch is up to date with 'origin/55-load-texture-images-into-memory-png-or-tga-and-normalize-points-between-0-1'.
     Changes to be committed:
    	modified:   Engine_(VS_Code_Project)/unit_tests/load_image.cpp
    	deleted:    Engine_(VS_Code_Project)/unit_tests/rasterizer_test.cpp
    - setup load_image.cpp file to initiate SDL screen.  Prepared for step
      of using SDL_image to load an image into memory

--- Engine_(VS_Code_Project)/unit_tests/load_image.cpp
+++ Engine_(VS_Code_Project)/unit_tests/load_image.cpp
@@ -4,6 +4,20 @@
 
 
 int main (int argv, char** args){
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+	window = SDL_CreateWindow("3D Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 380, 0);
+	renderer = SDL_CreateRenderer(window, -1, 0);
+    bool game_is_running=true;
+    while(game_is_running){
+        SDL_Event event;
+        while (SDL_PollEvent(&event)){
+            if(event.type==SDL_QUIT){
+                game_is_running=false;
+            }
+        }
+    }
+
 
     return 0;
 }
